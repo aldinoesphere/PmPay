d529ea318212da9e8e5ad28869a81adc5f0a771e
Aldino Esphere <denny.sutrisno@esphere.id>
2017-10-24 14:11:02 +0700

    Just Commit

--- src/Controllers/SettingsController.php
+++ src/Controllers/SettingsController.php
@@ -11,12 +11,7 @@ class SettingsController extends Controller
     public function loadConfiguration(Twig $twig):string
     {
         return $twig->render(
-        		'PmPay::Settings.Configuration',
-        		[
-					'status' => $this->request->get('status'),
-					'locale' => substr($_COOKIE['plentymarkets_lang_'], 0, 2),
-					'plentyId' => $plentyId,
-					'settingType' => $settingType				]
+        		'PmPay::Settings.Configuration'
         	);
     }
 }
