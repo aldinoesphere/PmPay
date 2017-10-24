319f699304230266c32c99023fe7da8db051b19e
Aldino Esphere <denny.sutrisno@esphere.id>
2017-10-24 11:42:50 +0700

    Just Commit

--- src/Controllers/SettingsController.php
+++ src/Controllers/SettingsController.php
@@ -1,63 +1,15 @@
-<?php  
+<?php
+ 
 namespace PmPay\Controllers;
-
+ 
+ 
 use Plenty\Plugin\Controller;
-use Plenty\Plugin\Http\Request;
-use Plenty\Plugin\Http\Response;
-use Plenty\Plugin\Application;
 use Plenty\Plugin\Templates\Twig;
-use Plenty\Modules\Frontend\Services\SystemService;
-
-/**
-* @package PmPay\Controllers
-*/
+ 
 class SettingsController extends Controller
 {
-
-	/**
-	 * @var Request
-	 */
-	private $request;
-
-	/**
-	 * @var Response
-	 */
-	private $response;
-
-	/**
-	 *
-	 * @var systemService
-	 */
-	private $systemService;
-
-	public function __construct(
-		Request $request,
-		Response $response,
-		SystemService $systemService
-	) {
-		$this->request = $request;
-		$this->response = $response;
-		$this->systemService = $systemService;
-	}
-
-
-	/**
-	 * Display PmPay backend configuration
-	 *
-	 * @param Twig $twig
-	 * @param string $settingType
-	 * @return void
-	 */
-	public function loadConfiguration(Twig $twig, $settingType)
-	{
-		// $plentyId = $this->systemService->getPlentyId();
-
-		return $twig->render('PmPay::Configuration.Settings');
-	}
-
-	public function saveConfiguration() {
-
-	}
+    public function loadConfiguration(Twig $twig):string
+    {
+        return $twig->render('PmPay::Configuration.Settings');
+    }
 }
-
-?>
--- src/Providers/PmPayRouteServiceProvider.php
+++ src/Providers/PmPayRouteServiceProvider.php
@@ -3,27 +3,14 @@ namespace PmPay\Providers;
 
 use Plenty\Plugin\RouteServiceProvider;
 use Plenty\Plugin\Routing\Router;
-// use Plenty\Plugin\Routing\ApiRouter;
 
 
-/**
-* @package PmPay\Providers
-*/
 class PmPayRouteServiceProvider extends RouteServiceProvider
 {
-	/**
-	* mapping the router
-	*
-	* @param Router $router
-	* @param ApiRouter $apiRouter
-	*/
 	public function map(Router $router) {
 
 		// Routes for display Skrill settings
 		$router->get('pmpay', 'PmPay\Controllers\SettingsController@loadConfiguration');
-
-		// Routes for save Skrill settings
-		$router->post('pmpay/settings/save', 'PmPay\Controllers\SettingsController@saveConfiguration');
 	}
 }
 
