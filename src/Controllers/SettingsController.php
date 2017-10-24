caeb9e1c7f6269dd7a36ff7c1b1125233fa3489a
Aldino Esphere <denny.sutrisno@esphere.id>
2017-10-24 14:27:14 +0700

    Just Commit

--- src/Controllers/SettingsController.php
+++ src/Controllers/SettingsController.php
@@ -4,14 +4,54 @@ namespace PmPay\Controllers;
  
  
 use Plenty\Plugin\Controller;
+use Plenty\Plugin\Http\Request;
+use Plenty\Plugin\Http\Response;
+use Plenty\Plugin\Application;
+use Plenty\Plugin\Log\Loggable;
 use Plenty\Plugin\Templates\Twig;
+use Plenty\Modules\Frontend\Services\SystemService;
  
 class SettingsController extends Controller
 {
+	use Loggable;
+
+	/**
+	 * @var Request
+	 */
+	private $request;
+
+	/**
+	 * @var Response
+	 */
+	private $response;
+
+	/**
+	 *
+	 * @var systemService
+	 */
+	private $systemService;
+
+	public function __construct(
+		Request $request,
+		Response $response,
+		SystemService $systemService
+	) {
+		$this->request = $request;
+		$this->response = $response;
+		$this->systemService = $systemService;
+	}
+
     public function loadConfiguration(Twig $twig):string
     {
+    	$plentyId = $this->systemService->getPlentyId();
+
+
         return $twig->render(
-        		'PmPay::Settings.Configuration'
+        		'PmPay::Settings.Configuration',
+        		[
+        			'plentyId' => $plentyId,
+        			'settingType' => 'HelloWorld'
+        		]
         	);
     }
 }
