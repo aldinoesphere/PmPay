e7600ba61e7eb3fd9d21fec88677ccbffd7de1b4
Denny Sutrisno <denny.sutrisno@esphere.id>
2017-10-24 10:39:27 +0700

    Just Commit

---
+++ src/Controllers/SettingsController.php
@@ -0,0 +1,74 @@
+<?php  
+namespace PmPay\Controllers;
+
+use Plenty\Plugin\Controller;
+use Plenty\Plugin\Http\Request;
+use Plenty\Plugin\Http\Response;
+use Plenty\Plugin\Application;
+use Plenty\Plugin\Log\Loggable;
+use Plenty\Plugin\Templates\Twig;
+use Plenty\Modules\Frontend\Services\SystemService;
+
+/**
+* @package PmPay\Controllers
+*/
+class SettingsController extends Controller
+{
+	use Loggable;
+
+	/**
+	 * @var Request
+	 */
+	private $request;
+
+	/**
+	 * @var Response
+	 */
+	private $response;
+
+	/**
+	 *
+	 * @var systemService
+	 */
+	private $systemService;
+
+	public function __construct(
+		Request $request,
+		Response $response,
+		SystemService $systemService,
+	) {
+		$this->request = $request;
+		$this->response = $response;
+		$this->systemService = $systemService;
+	}
+
+
+	/**
+	 * Display Skrill backend configuration
+	 *
+	 * @param Twig $twig
+	 * @param string $settingType
+	 * @return void
+	 */
+	public function loadConfiguration(Twig $twig, $settingType)
+	{
+		$plentyId = $this->systemService->getPlentyId();
+
+		return $twig->render(
+						'PmPay::Configuration.Settings',
+						array(
+							'status' => $this->request->get('status'),
+							'locale' => substr($_COOKIE['plentymarkets_lang_'], 0, 2),
+							'plentyId' => $plentyId,
+							'settingType' => $settingType,
+							'setting' => $configuration
+						)
+		);
+	}
+
+	public function saveConfiguration() {
+
+	}
+}
+
+?>
---
+++ src/Providers/PmPayRouteServiceProvider.php
@@ -0,0 +1,30 @@
+<?php  
+namespace PmPay\Providers;
+
+use Plenty\Plugin\RouteServiceProvider;
+use Plenty\Plugin\Routing\Router;
+use Plenty\Plugin\Routing\ApiRouter;
+
+
+/**
+* @package PmPay\Providers
+*/
+class PmPayRouteServiceProvider extends RouteServiceProvider
+{
+	/**
+	* mapping the router
+	*
+	* @param Router $router
+	* @param ApiRouter $apiRouter
+	*/
+	public function map(Router $router, ApiRouter $apiRouter) {
+
+		// Routes for display Skrill settings
+		$router->get('pmpay/settings/{settingType}', 'PmPay\Controllers\SettingsController@loadConfiguration');
+
+		// Routes for save Skrill settings
+		$router->post('pmpay/settings/save', 'PmPay\Controllers\SettingsController@saveConfiguration');
+	}
+}
+
+?>
