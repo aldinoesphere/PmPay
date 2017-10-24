b42fc15ca8557add5c4038c6d35894f59c0c4625
Aldino Esphere <denny.sutrisno@esphere.id>
2017-10-24 15:34:18 +0700

    Just Commit

---
+++ src/Models/Database/Settings.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace PmPay\Models\Database;
+
+use Plenty\Modules\Plugin\DataBase\Contracts\Model;
+
+/**
+* Class Settings
+* @package PmPay\Models\Database
+*
+* @property int $id
+* @property int $webstore
+* @property string $name
+* @property array $value
+* @property string $createdAt
+* @property string $updatedAt
+*/
+class Settings extends Model
+{
+
+	public $id = 0;
+	public $webstore = 0;
+	public $name = '';
+	public $value = array();
+	public $createdAt = '';
+	public $updatedAt = '';
+
+	/**
+	 * get table name
+	 *
+	 * @return string
+	 */
+	public function getTableName()
+	{
+		return 'PmPay::settings';
+	}
+}
