e380a5253f54a167f447f25686af6d98dba7ebc5
Aldino Esphere <denny.sutrisno@esphere.id>
2017-10-25 10:41:26 +0700

    Just Commit

--- src/Services/Database/SettingsService.php
+++ src/Services/Database/SettingsService.php
@@ -149,25 +149,21 @@ class SettingsService extends DatabaseBaseService
      * set initial settings payment method name for each plentyId
      *
      */
-    // public function setInitialSettings()
-    // {
-    //     $clients = $this->getClients();
-    //     $paymentMethods = Settings::AVAILABLE_PAYMENT_METHODS;
+    public function setInitialSettings()
+    {
+        $clients = $this->getClients();
 
-    //     foreach ($clients as $plentyId)
-    //     {
-    //         foreach ($paymentMethods as $key => $value)
-    //         {
-    //             $settings = array();
-    //             $settings[] = array(
-    //                 'PID_'.$plentyId => array(
-    //                     'language' => $value
-    //                 )
-    //             );
-    //             $this->saveSettings($key, $settings);
-    //         }
-    //     }
-    // }
+        foreach ($clients as $plentyId)
+        {
+            $settings = array();
+            $settings[] = array(
+                'PID_'.$plentyId => array(
+                    'language' => $value
+                )
+            );
+            $this->saveSettings('pmpay_settings', $settings);
+        }
+    }
 
     /**
      * get PmPay configuration by plentyId and settingType
